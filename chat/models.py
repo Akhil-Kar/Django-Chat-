b72a151f6a61660dbac218a2f3258a821cbb2e92
akhil kar <akhilkar@ternaengg.ac.in>
2021-06-11 23:21:25 +0530

    chat code upload

---
+++ chat/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+class Message(models.Model):
+    value = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.CharField(max_length=200)
+    room = models.CharField(max_length=200)
